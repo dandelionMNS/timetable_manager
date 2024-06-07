07cfdbbc8becfcffa8e04a7ddae4c4c3b2fdaf0f
najmusyathir <alsyathir@gmail.com>
2024-06-08 03:45:44 +0800

    Add: Update database

---
+++ database/migrations/2024_06_07_180259_update_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('user_type')->default('student')->change();
+            $table->string('matric_no')->unique()->nullable();
+            $table->string('phone_no')->nullable();
+            $table->foreignId('batch_id')->nullable()->constrained('batches')->onDelete('cascade');
+            $table->foreignId('course_id')->nullable()->constrained('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('user_type')->default('user')->change();
+            $table->dropColumn('matric_no');
+            $table->dropColumn('phone_no');
+            $table->dropForeign(['batch_id']);
+            $table->dropColumn('batch_id');
+            $table->dropForeign(['course_id']);
+            $table->dropColumn('course_id');
+        });
+    }
+};
