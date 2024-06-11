d8dddc61b1be2e9a79001b529d6f517b91c70aae
najmusyathir <alsyathir@gmail.com>
2024-06-11 23:14:57 +0800

    Modify: Relocate instructor from subject to assigned_class

--- database/migrations/2024_06_07_122441_create_subjects_table.php
+++ database/migrations/2024_06_07_122441_create_subjects_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('code');            
-            $table->foreignId('instructor')->constrained('users')->onDelete('cascade');
             $table->integer('credit_hour');
 
         });
